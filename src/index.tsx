95824a67693ad866bb722dd804233fcad3a2a994
Steven Harding <sharding1012@gmail.com>
2024-02-28 18:33:22 -0800

    fix eslint issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import { connect } from 'datocms-plugin-sdk';
+
 import { render } from './utils/render';
 import ConfigScreen from './entrypoints/ConfigScreen';
 import 'datocms-react-ui/styles.css';
