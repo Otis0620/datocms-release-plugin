8bae7554724d26478d9f6c18faa63270c7b9c3f6
Steven Harding <sharding1012@gmail.com>
2024-02-28 19:08:35 -0800

    add navigation tab and initial release page

---
+++ src/entrypoints/Pages/Release/index.tsx
@@ -0,0 +1,11 @@
+import { RenderPageCtx } from 'datocms-plugin-sdk';
+
+type Props = {
+  ctx: RenderPageCtx;
+};
+
+const Release = ({ ctx }: Props) => {
+  return <h1>Release</h1>;
+};
+
+export default Release;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,40 @@
-import { connect } from 'datocms-plugin-sdk';
+import {
+  IntentCtx,
+  RenderConfigScreenCtx,
+  RenderPageCtx,
+  connect,
+} from 'datocms-plugin-sdk';
 
 import { render } from './utils/render';
 import ConfigScreen from './entrypoints/ConfigScreen';
 import 'datocms-react-ui/styles.css';
+import Release from './entrypoints/Pages/Release';
+
+enum PageId {
+  Release = 'release',
+}
 
 connect({
-  renderConfigScreen(ctx) {
+  renderConfigScreen(ctx: RenderConfigScreenCtx) {
     return render(<ConfigScreen ctx={ctx} />);
+  },
+
+  renderPage(pageId: string, ctx: RenderPageCtx) {
+    switch (pageId) {
+      case PageId.Release:
+        return render(<Release ctx={ctx} />);
+    }
+  },
+
+  mainNavigationTabs(ctx: IntentCtx) {
+    return [
+      {
+        label: 'Release',
+        icon: 'starship',
+        pointsTo: {
+          pageId: PageId.Release,
+        },
+      },
+    ];
   },
 });
